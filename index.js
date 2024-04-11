57b1650821420264a29a54c2e39d20cbddce5dd3
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2024-04-12 03:40:19 +0530

    final product

---
+++ index.js
@@ -0,0 +1,12 @@
+function add(){
+    let adda=document.querySelector('#adda');
+    let val=parseInt(adda.innerText);
+    val++;
+    adda.innerText=val;
+}
+function sub(){
+    let adda=document.querySelector('#adda');
+    let val=parseInt(adda.innerText);
+    val--;
+    adda.innerText=val;
+}
